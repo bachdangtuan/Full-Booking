a6aed4497e20670052b22066ff07c78848cd94f0
Tuan Bach <tuan.bach@infonam.com>
2022-05-01 20:01:32 +0700

    update

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -1,53 +1,99 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { layChiTietPhongVeAction } from '../../redux/actions/QuanLyDatVeAction';
+import { DAT_GHE } from '../../redux/actions/TypeAction/TypeActionQuanLyDatVe';
+import style from '../Checkout/Checkout.module.css'
 
 export default function Checkout(props) {
+  const dispatch = useDispatch();
 
   const { userLogin } = useSelector(state => state.QuanLyNguoiDungReducer)
-  console.log(userLogin);
 
 
+  //Chi Tiết phòng vé
+  const { chiTietPhongVe, danhSachGheDat } = useSelector(state => state.QuanLyDatVeReducer)
+  const { thongTinPhim, danhSachGhe } = chiTietPhongVe;
 
-  const chiTietPhongVe = useSelector(state => state.QuanLyDatVeReducer)
+  //Danh Sách đặt ghế
+  console.log(danhSachGheDat);
 
 
-  const dispatch = useDispatch();
-  
-  useEffect(() =>{
+  useEffect(() => {
     console.log(props.match.params.id);
-
-
     const action = layChiTietPhongVeAction(props.match.params.id)
     dispatch(action)
-  },[])
+  }, [])
 
-  console.log(chiTietPhongVe);
 
+  const renderGhe = () => {
+    return danhSachGhe?.map((ghe, index) => {
+      const classGheVip = (ghe.loaiGhe === 'Vip' ? 'gheVip' : '');
+      const classDaDat = (ghe.daDat === true ? 'gheDaDat' : '');
+
+      let classDangDat = '';
+      let indexDD = danhSachGheDat.findIndex(gheDD => gheDD.maGhe === ghe.maGhe)
+      if (indexDD != -1) {
+        classDangDat = 'gheDangDat';
+      }
+
+      return <Fragment key={index}>
+        <button
+          onClick={() => {
+            dispatch({
+              type: DAT_GHE,
+              gheDuocChon: ghe
+            })
+          }
+          }
+          disabled={ghe.daDat} className={`${style['ghe']} ${style[classGheVip]} ${style[classDaDat]} ${style[classDangDat]} cursor-pointer`}>
+          {ghe.stt}
+        </button>
+        {(index + 1) % 16 === 0 ? <br /> : ''}
+      </Fragment>
+    })
+  }
+
+  const renderGheDat = () => {
+    return danhSachGheDat.map((sp, index) => {
+      return <span key={index} className='ml-5'>{sp.tenGhe}</span>
+    })
+  }
+
+
+
+  const tinhTienVe = () => {
+    let tongGia = 0;
+    for (let i = 0; i < danhSachGheDat.length; i++) {
+      let gia = danhSachGhe[i].giaVe
+      tongGia += gia
+    }
+    return <span>{tongGia}</span>
+  }
 
   return (
-    <div className='container'>
+    <div className='container pt-10'>
       <div className='grid grid-cols-12'>
-        <div className='col-span-8'>
-
+        <div className='col-span-9 m-auto'>
+          <h1 className='text-center'>GHẾ </h1>
+          {renderGhe()}
         </div>
-        <div className='col-span-4'>
+        <div className='col-span-3'>
           <h3 className='text-green-400 text-center'>0 đ</h3>
           <hr />
-          <h3>Lật Mặt 48h</h3>
-          <p>Địa Điểm:</p>
-          <p>Ngày Chiếu</p>
+          <h3>{thongTinPhim?.tenPhim}</h3>
+          <p>Địa Điểm:{thongTinPhim?.diaChi}</p>
+          <p>Ngày Chiếu: {thongTinPhim?.ngayChieu}</p>
           <hr />
-          <p>Ghế</p>
-          <p>0đ</p>
+          <p>Ghế {renderGheDat()} </p>
+          <p>Tính Tiền: {tinhTienVe()} </p>
           <hr />
           <h3>Email: {userLogin.email} </h3>
           <hr />
           <h3>Số Điện Thoại: {userLogin.soDT}</h3>
           <div className='mb-0 flex flex-col justify-end items-center'>
-              <div className='bg-green-500 text-white w-full text-center font-bold'>
-                  Đặt Vé
-              </div>
+            <div className='bg-green-500 text-white w-full text-center font-bold cursor-pointer'>
+              Đặt Vé
+            </div>
           </div>
         </div>
       </div>
--- src/redux/reducers/QuanLyDatVeReducer.jsx
+++ src/redux/reducers/QuanLyDatVeReducer.jsx
@@ -1,22 +1,53 @@
-import { SET_CHI_TIET_PHONG_VE } from "../actions/TypeAction/TypeActionQuanLyDatVe"
+import { DAT_GHE, SET_CHI_TIET_PHONG_VE } from "../actions/TypeAction/TypeActionQuanLyDatVe"
 
 const stateDefault = {
-    chiTietPhongVe: {}
+    chiTietPhongVe: {},
 
+
+    danhSachGheDat: [
+        // {
+        //     "maGhe": 47427,
+        //     "tenGhe": "27",
+        //     "maRap": 451,
+        //     "loaiGhe": "Thuong",
+        //     "stt": "27",
+        //     "giaVe": 100000,
+        //     "daDat": false,
+        //     "taiKhoanNguoiDat": null
+        // }
+    ]
 }
 
 export const QuanLyDatVeReducer = (state = stateDefault, action) => {
     switch (action.type) {
-            case SET_CHI_TIET_PHONG_VE:{
-                state.chiTietPhongVe=action.chiTietPhongVe
+        case SET_CHI_TIET_PHONG_VE: {
+            state.chiTietPhongVe = action.chiTietPhongVe
+            return { ...state }
+        }
 
+        case DAT_GHE: {
+            const danhSachGheDatUpdate = [...state.danhSachGheDat]
 
+            console.log("action", action.gheDuocChon.tenGhe);
+            let maGheChon = action.gheDuocChon.maGhe
+            let index = danhSachGheDatUpdate.findIndex(gheDD => gheDD.maGhe === maGheChon)
 
-                return {...state}
+            if (index != -1) {
+                danhSachGheDatUpdate.splice(index, 1)
+                state.danhSachGheDat = danhSachGheDatUpdate;
+            } else {
+                danhSachGheDatUpdate.push(action.gheDuocChon)
+                state.danhSachGheDat = danhSachGheDatUpdate;
             }
+            console.log(index);
+            //Click lần 2 thì xóa   
 
-            
-        
+            console.log("danhSachGheDatUpdate", danhSachGheDatUpdate);
+
+            return { ...state }
+        }
+
+
         default: return { ...state }
     }
 }
